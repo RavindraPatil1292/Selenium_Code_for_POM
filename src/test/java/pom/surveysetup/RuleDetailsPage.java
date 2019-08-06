101e43fff15b85c2ee8485fd14ad8c65e080c4c5
Ravindra Patil <RaPatil@eagle.org>
2019-08-06 15:09:18 +0530

    removed src

--- src/test/java/pom/surveysetup/RuleDetailsPage.java
+++
@@ -1,9 +0,0 @@
-package pom.surveysetup;
-
-import org.testng.annotations.Test;
-
-public class RuleDetailsPage {
-  @Test
-  public void f() {
-  }
-}
--- src/test/java/pom/surveysetup/SurveyListPage.java
+++
@@ -1,9 +0,0 @@
-package pom.surveysetup;
-
-import org.testng.annotations.Test;
-
-public class SurveyListPage {
-  @Test
-  public void f() {
-  }
-}
